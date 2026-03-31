88ced074eafe36709f663630390d91da4d22018a
joacocaturelli <joacocatu@gmail.com>
2026-03-31 16:23:23 -0300

    Añadido boton para agregar elementos y otro para quitarlos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
  // Aquí tu código
+const lista = document.getElementById("lista");
+const agregar = document.getElementById("agregar");  
+
+agregar.addEventListener("click", () => {
+    const newElement = prompt("Ingrese el nuevo elemento:");
+    if (newElement) {
+        const li = document.createElement("li");
+        li.textContent = newElement;
+        lista.appendChild(li);
+    }
+});
+
+const eliminar = document.getElementById("eliminar");
+
+eliminar.addEventListener("click", () => {
+    const lastElement = lista.lastChild;
+    if (lastElement) {
+        lista.removeChild(lastElement);
+    }
+});
